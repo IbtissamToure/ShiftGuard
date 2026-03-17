0bb686f7f39983475270c0791fe72867261a1cf4
Ibtissam Toure <ibtissamtoure@MacBookAir>
2026-03-17 16:59:08 +0300

    added rest hours calculations and danger detection

--- src/anlyzer.py
+++ src/anlyzer.py
@@ -2,4 +2,17 @@ import pandas as pd
 df = pd.read_csv("data/Shifts.csv")
 df["shift_start"] = pd.to_datetime(df["shift_start"])
 df["shift_end"] = pd.to_datetime(df["shift_end"])
-print(df)
+def calculate_rest_hours(df):
+    results = []
+    for employee, shifts in df.groupby("employee_name"):
+        shifts = shifts.sort_values("shift_start") #sort the values تصاعديا
+
+        for i in range(len(shifts) - 1):
+            current_end = shifts.iloc[i]["shift_end"]
+            next_start = shifts.iloc[i + 1]["shift_start"]
+
+            rest_hours = (next_start - current_end).total_seconds() /3600
+            results.append({"Employee" : employee, "Rest Hours": rest_hours,"Is dangerous" : rest_hours < 11})
+    return pd.DataFrame(results)
+rest_def = calculate_rest_hours(df)
+print(rest_def)
