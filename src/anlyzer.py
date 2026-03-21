738dc85af4a7db6c689facb3e1c0334713312ba4
Ibtissam Toure <ibtissamtoure@MacBookAir>
2026-03-21 21:16:36 +0300

    added a risk score

--- src/anlyzer.py
+++ src/anlyzer.py
@@ -2,6 +2,10 @@ import pandas as pd
 df = pd.read_csv("data/Shifts.csv")
 df["shift_start"] = pd.to_datetime(df["shift_start"])
 df["shift_end"] = pd.to_datetime(df["shift_end"])
+def calculate_risk_score(rest_hours):
+    score = 100 - ((rest_hours / 24) *100)
+    return round(max(0,min(100, score)),1)
+
 def calculate_rest_hours(df):
     results = []
     for employee, shifts in df.groupby("employee_name"):
@@ -12,9 +16,10 @@ def calculate_rest_hours(df):
             next_start = shifts.iloc[i + 1]["shift_start"]
 
             rest_hours = (next_start - current_end).total_seconds() /3600
-            results.append({"Employee" : employee, "Rest Hours": rest_hours,"Is dangerous" : rest_hours < 11})
+            results.append({"Employee" : employee, "Rest Hours": rest_hours,"Is dangerous" : rest_hours < 11, "Risk Score" : calculate_risk_score(rest_hours)})
     return pd.DataFrame(results)
 rest_df = calculate_rest_hours(df)
+
 def suggest_replacement(rest_df,employee_to_replace):
     safe_employees = rest_df[rest_df["Is dangerous"] == False]
     safe_employees = safe_employees.sort_values("Rest Hours", ascending = False)
@@ -23,5 +28,9 @@ def suggest_replacement(rest_df,employee_to_replace):
         return "لا يوجد موظف آمن متاح"
     best = safe_employees.iloc[0]
     return f"الاقتراح:{ best['Employee']} - راحة: {best['Rest Hours']} ساعة"
-print(suggest_replacement(rest_df, "Sara Ahmed"))
+
+def calculate_risk_score(rest_hours):
+    score = 100 - ((rest_hours / 24) *100)
+    return round(max(0,min(100, score)),1)
+print(calculate_rest_hours(df))
  
